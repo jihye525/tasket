b5800434256665feb6aebc7b0d41e70efa90e75c
woojin <ooojin523@gmail.com>
2025-03-09 16:53:14 +0900

    프로젝트 설정 초기화
    tasket 프로젝트 설정 초기화하였습니다.

---
+++ src/main/java/com/woojhye/tasket/TasketApplication.java
@@ -0,0 +1,13 @@
+package com.woojhye.tasket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasketApplication.class, args);
+	}
+
+}
