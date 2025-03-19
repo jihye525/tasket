5d99b0ac833044d6d9e54404ed5ecfc8c13a2534
jihye <jihye07201@gmail.com>
2025-03-19 17:12:21 +0900

    [BE] feat: 유저, 베이스 엔티티 생성
    [BE] feat: 유저, 베이스 엔티티 생성

--- src/main/java/com/woojhye/tasket/user/domain/User.java
+++ src/main/java/com/woojhye/tasket/user/domain/User.java
@@ -1,4 +1,30 @@
 package com.woojhye.tasket.user.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
+@Entity
 public class User {
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Id
+    private Long userId;
+
+    @Column(nullable = false, unique = true, updatable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String nickname;
+
+    private String profile;
+
 }
+
